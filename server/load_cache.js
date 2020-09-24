16f57bf11cf1e620e1428451ffd1c8d241b7a32a
Walter Corrales <corraleswalter@live.com>
2020-09-23 23:53:54 -0600

    linter suggestions

--- server/load_cache.js
+++ server/load_cache.js
@@ -1,20 +1,16 @@
-const { promisify } = require("util");
-const resources = require('./resources');
+const {promisify} = require("util");
+const resources = require("./resources");
 const log = require("./log.js");
 const cfg = require("./cfg.js");
 const argv = require("minimist")(process.argv.slice(2), {
   boolean: ["color", "d", "daemon", "dev"]
 });
 
-let cache = {};
 let config = null;
-
-const cmd  = argv._[0];
 const args = argv._.slice(1);
 
 (async function loadCache(dev = {}) {
   try {
-
     await promisify((cb) => {
       cfg.init(null, (err, conf) => {
         if (!err) {
@@ -27,9 +23,9 @@ const args = argv._.slice(1);
 
     await promisify((cb) => {
       log.info("Loading resources ...");
-      resources.load(args.dev, (err, c) => {
+      resources.load(args.dev, (err) => {
         log.info("Loading resources done");
-        cache = c; cb(err);
+        cb(err);
       });
     })();
   } catch (error) {
